1cb2eb2738a09a4ef3bf96b43a23cec93aaef54a
Harinarayan <144616668+HarinarayanJ@users.noreply.github.com>
2026-04-18 08:23:52 -0400

    settings

---
+++ utils/settings.py
@@ -0,0 +1,13 @@
+PERSISTANCE=0.2
+STABILITY=0.5
+USERNAME="root"
+PASSWORD="root"
+HOST="localhost"
+PORT="3306"
+DB="Test"
+
+MONGODB_USERNAME="test"
+MONGODB_PASSWORD="password"
+CLUSTER="cluster0.xxxxxx.mongodb.net"
+
+CONNECTION="mongodb://localhost:27017/"          
